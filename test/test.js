2ab03936a39eacf7ca663211636c8de857eef97b
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2023-07-29 16:25:32 +0100

    Fix broken tests - require knex in beforeEach

--- test/test.js
+++ test/test.js
@@ -4,26 +4,21 @@ const { expect } = require('@hapi/code')
 const doStuff = require('../do-stuff')
 
 describe('Test', () => {
+  let knex
   beforeEach(() => {
+    knex = require('knex')({
+      client: 'pg',
+      debug: true,
+      connection: process.env.LFW_DATA_DB_CONNECTION
+    })
   })
   afterEach(() => {
   })
   it('Test 1', async () => {
-    const knex = require('knex')({
-      client: 'pg',
-      debug: true,
-      connection: process.env.LFW_DATA_DB_CONNECTION
-    })
-    await doStuff(knex)
     const result = await doStuff(knex)
     expect(result.length).to.equal(5)
   })
   it('Test 2', async () => {
-    const knex = require('knex')({
-      client: 'pg',
-      debug: true,
-      connection: process.env.LFW_DATA_DB_CONNECTION
-    })
     const result = await doStuff(knex)
     expect(result.length).to.equal(5)
   })
